7a73ffcc2143b06fd8433e2202cc2e974c15fbd8
Fardous <fardousalom42@gmail.com>
2024-08-21 23:49:33 +0600

    project setup complete

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import SignInPage from './auth/sign-in/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+  path: "/auth/sign-in",
+  element:<SignInPage />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <RouterProvider router={router} />
     <App />
   </StrictMode>,
 )
